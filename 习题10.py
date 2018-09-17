58b1b1d8e0c84d6dca0422a47849e14991227cc6
json <547816252@qq.com>
2018-09-17 23:09:25 +0800

    练习10

---
+++ 习题10.py
@@ -0,0 +1,17 @@
+today_cat = "\tI'm tabbed in."
+persian_cat = "I'm solit\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+bell = "\ahahha"
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(today_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(bell)
