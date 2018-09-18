3990589790e7e7ea23f7369c09d42939c1f61d82
json <547816252@qq.com>
2018-09-18 08:33:22 +0800

    input('请输入……')提示别人输入

---
+++ 习题12.py
@@ -0,0 +1,8 @@
+age = input()
+height = input("How tall are you? ")
+weight = input("How much are you weight? ")
+
+
+print("So, you're {0} old, {1} tall and {2} heavy.".format(age,height,weight))
+
+print("How tall are you?", input())
