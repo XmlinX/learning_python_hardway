2d165e37b6dff7a848a3e737b6be2c9c2595a47c
json <547816252@qq.com>
2018-09-22 22:13:35 +0800

    if 语句

---
+++ 习题26.py
@@ -0,0 +1,103 @@
+#打印（“你多大了？”，结束=''）
+print(“你多大了？”，and='')
+#年龄=输入（）
+age = input("请输入年龄：")
+
+#打印（“你有多高？”，结束=''）
+print("How high you are ?", end='')
+print(“你的体重多少？”，结束='')
+
+weight = input()
+
+print(f"所以，你是 {age} old，{height} tall和{weight} heavy.")
+
+script，filename = argv
+
+txt = open(filenme)
+
+print("这是你的文件{filename}：")
+print(tx.read())
+
+print("再次输入文件名: ")
+file_again = input(">")
+
+txt_again = open(file_again)
+
+print(txt_again_read())
+
+
+打印（'让我们练习一切。'）
+打印（'你需要知道'回合逃脱 
+      用\\做\ n换行和\ t制表符。'）
+
+诗=“”“
+\ t可爱的世界
+逻辑如此坚定地种植
+无法辨别爱的需要
+也不理解直觉的激情
+并需要解释
+\ n \ t \ twhere没有。
+“””
+
+打印（” -  -  -  -  -  -  - ）
+打印（诗）
+打印（ -  -  -  -  -  -  - ”）
+
+
+五 = 10  -  2 + 3  - 0  
+print(f"这应该是五：{five}")
+
+def secret_formula（已启动）
+    jelly_beans = 开始 * 500
+    jars = jelly_beans / 1000
+    crates = jars 100
+    return jelly_beans，罐子，板条箱
+
+
+start_point = 10000
+beans，jars = secret_formula（start_point）
+
+＃记住这是格式化字符串的另一种方法
+print（"起点为：{}".format（start_point））
+＃就像f“”字符串一样
+print（f"我们有{beans}豆，{jars}罐子和{crates}板条箱."）
+
+start_point = start_point / 10
+
+print("我们也可以这样做："）
+formula = secret_formula(startpoint)
+＃这是将列表应用于格式字符串的简便方法
+print("我们有豆，{} 罐子和{}板条箱".format(*formula)
+
+
+
+人 = 20
+cates = 30
+狗 = 15
+
+
+如果人<猫：
+    打印“猫太多了！世界注定要失败！”
+
+如果人<猫：
+    打印（“没有多少猫！世界得救了！”）
+
+如果人们<狗：
+    打印（“世界在流淌！”）
+
+如果人>狗
+    打印（“这个世界很干！”）
+
+
+狗+ = 5
+
+如果人> =狗：
+    打印（“人们大于或等于狗。”）
+
+如果人们<=狗
+    打印（“人们小于或等于狗。”
+
+
+如果人们=狗：
+    打印（“人是狗。”）
+
